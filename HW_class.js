2d16de8e52f53467a277957364dce545c233aa54
Eli Clark <77750838+okawaffles@users.noreply.github.com>
2022-08-24 08:35:06 -0500

    lobby class

--- HW_class.js
+++ HW_class.js
@@ -11,4 +11,33 @@ class HWUser {
     }
 }
 
-module.exports = {HWUser};
+let lobbyIdCounter = 0;
+
+class Lobby {
+    constructor(starterUserId, settings) {
+        this.suid = starterUserId;
+        this.settings_json = settings;
+        this.name = settings.name;
+        this.playerlimit = settings.pl;
+        this.continual = settings.continual;
+
+        this.players = [starterUserId];
+    }
+
+    get lobbyDetails() {
+        return this.settings_json;
+    }
+    get playerList() {
+        return this.players;
+    }
+
+    get joinPlayer(pid) {
+        this.players.push(pid);
+    }
+    get removePlayer(pid) {
+        let index = this.players.indexOf(pid);
+        if (index > -1) this.players.splice(index, 1);
+    }
+}
+
+module.exports = {HWUser, Lobby};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const ws = require('ws'); //websockets
 const express = require('express'); //webserver
-const { HWUser } = require('./HW_class.js'); 
+const { HWUser, Lobby } = require('./HW_class.js'); 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 
+let lobbies = [];
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
@@ -28,6 +29,15 @@ wss.on('connection', function connection(ws) {
         if (dat.mt == "hello") {
             user = new HWUser(dat.id);
             ws.send(user.ackHello());
+        }
+        if (dat.mt == "jl") {
+            user = new HWUser(dat.id);
+            if (!lobbies[dat.lid]) {
+                ws.send(JSON.stringify({result:"fail",reason:"Lobby does not exist."}));
+                return;
+            } else {
+                
+            }
         }
     });
   
